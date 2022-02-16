204c0860a1bb630dedbbf45212aa99d696515500
Your Name <mari@Maribels-MacBook-Pro.local>
2022-02-15 20:26:20 -0600

    linked all movies and descriptions are available to view

--- src/index.js
+++ src/index.js
@@ -10,7 +10,6 @@ import './index.css';
 import reducer from './reducers';
 const store = createStore(
   reducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
 )
 
 ReactDOM.render(
